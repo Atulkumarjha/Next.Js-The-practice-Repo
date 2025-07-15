a6b667feed848c438b49b59b7de35bc1dde588e8
Atul kumar jha <jhaak301@gmail.com>
2025-07-15 16:19:54 +0530

    routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-export default function Page() {
-  return (
-    <h1>Hello! Next.Js</h1>
-  )
-}
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
+  const filters = (await searchParams).filters
+}``
