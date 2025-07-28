1e8d0108f22ec1eed179327bc92460b5fa71993f
Atul kumar jha <jhaak301@gmail.com>
2025-07-28 16:54:32 +0530

    initial commit

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -8,6 +8,6 @@ export default function BlogLayout({
             {children} 
         </section>
     )
-}
+} 
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}) {
+}) { 
   const filters = (await searchParams).filters
-}``
+}  
